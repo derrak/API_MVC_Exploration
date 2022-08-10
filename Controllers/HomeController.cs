3ea5207c6ddf4a9c16b99c66397d2f4f69b11d28
Derrak Richard <derrak@gmail.com>
2022-08-10 11:42:12 -0700

    add all files from lesson
    Co-authored-by: Alex Shevlin <alexshevlin1@gmail.com>
    Co-authored-by: Sandra Tena <sandratena06@gmail.com>

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using APIs_MVC.Models;
+
+namespace APIs_MVC.Controllers
+{
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
+    {
+      var allArticles = Article.GetArticles(EnvironmentVariables.ApiKey);
+      return View(allArticles);
+    }
+  }
+}
